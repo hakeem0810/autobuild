46da04796dcca65797592147739bd9ff7d406fa5
maying <13909314488@139.com>
2017-10-24 10:14:38 +0800

    initial

---
+++ src/main/java/cn/cmccgs/autobuild/AutobuildApplication.java
@@ -0,0 +1,12 @@
+package cn.cmccgs.autobuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobuildApplication.class, args);
+	}
+}
